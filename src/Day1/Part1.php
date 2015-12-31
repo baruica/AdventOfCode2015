e81633f649bdf24f854fa054178c8485698c6a98
Nelson da Costa <barui.ca@gmail.com>
2015-12-31 16:18:19 +0100

    fewer lines

--- src/Day1/Part1.php
+++ src/Day1/Part1.php
@@ -6,12 +6,10 @@ class Part1
 {
     public function __invoke()
     {
-        $instructions = file_get_contents(__DIR__ . '/../../input/day1.txt');
-
         $elevator = new Elevator();
         $parser = new InstructionParser($elevator);
 
-        $parser->parseInstructions($instructions);
+        $parser->parseInstructions(file_get_contents(__DIR__ . '/../../input/day1.txt'));
 
         echo 'Final floor : '.$elevator->getFloor()."\n\n";
     }
